e4bef097ab16f053013f962a88f5e5713f2cf20f
Mauricio Gomes <mgpensar@gmail.com>
2022-11-12 08:26:00 -0300

    Working on Object base class.
    Working on Object base class.

--- src/include/name.hpp
+++ src/include/name.hpp
@@ -1,78 +1,65 @@
-#ifndef NAMED_HPP_INCLUDED
-#define NAMED_HPP_INCLUDED
+#ifndef NAME_HPP
+#define NAME_HPP
 
-#include "string_def.hpp"
-#include "version.hpp"
-
-#include <string>
-#include <iostream>
+#include "object.hpp"
 
 namespace pensar_digital
 {
     namespace cpplib
     {
-        /// Inherit from ReadOnlyName to have a read only name atributte.
-        /// \tparam String The string class used to hold the name attribute.
-        template <class String = string>
-        class ReadOnlyName : public Version
-        {
-            protected:
-            ~ReadOnlyName (){};
-
-            public:
-            ReadOnlyName (const String& aname): Version(0), name(aname){};
-
-            /// Access name
-            /// \return The current value of name
-            String get_name () const { return name; }
-
-            bool operator == (const ReadOnlyName<String>& aname) const {return (aname.name == name);}
-            bool operator != (const ReadOnlyName<String>& aname) const {return (aname.name != name);}
-            using Version::operator <<;
-            using Version::operator >>;
-
-            virtual std::istream& ReadFromStream (std::istream& is, const Version::V v)
-            {
-                switch (v)
-                {
-                    case 0:
-                        return is >> name;
-                };
-                return is;
-            }
-
-            virtual std::ostream& WriteToStream (std::ostream& os, const Version::V v) const
-            {
-                switch (v)
-                {
-                    case 0:
-                        return os << name;
-                };
-                return os;
-            }
-
-            protected:
-            String name; //!< Member variable "name"
-       };
-
-        /// Inherit from Name to have a read/write name atributte.
-        /// \tparam String The string class used to hold the name attribute.
-        template <class String = string>
-        class Name : public ReadOnlyName<String>
+        class RO_Name: public virtual RO_Object
         {
             public:
-            Name (const String& aname): ReadOnlyName<String>(aname) {};
+                /** Access name;
+                 * \return The current value of name;
+                 */
+                std::string get_name () const { return name;}
+                bool equals (const RO_Name& n) const {return name == n.name;}
 
-            using ReadOnlyName<String>::operator ==;
-            using ReadOnlyName<String>::operator !=;
-            using ReadOnlyName<String>::operator <<;
-            using ReadOnlyName<String>::operator >>;
+                bool operator == (const RO_Name& n) const {return equals (n);}
 
-            /// Set name
-            /// \param val New value to set
-            void set_name (const String& val) { ReadOnlyName<String>::name = val; }
+            protected:
+                RO_Name () {}
+
+            private:
+                std::string name;
+        };
+
+        class Name : public RO_Name, public virtual Object
+        {
+            public:
+                /** Default constructor */
+                Name(): RO_Name(), Object() {}
+
+                /** Default destructor */
+                virtual ~Name() {}
+
+                /** Copy constructor
+                 *  \param n Object to copy from
+                 */
+                Name(const Name& n) {assign (n);};
+
+                using RO_Name::operator ==;
+
+                Name& assign (const Name& n) {Object::assign(n); name = n.name; return *this;}
+
+                /** Assignment operator
+                 *  \param n Object to assign from
+                 *  \return A reference to this
+                 */
+                Name& operator=(const Name& n) { return assign (n); }
+
+                /** Set name;
+                 * \param val New value to set
+                 */
+                void set_name (const std::string& n) { name = n; }
+
+            protected:
+
+            private:
+                std::string name; //!< Member variable "name"
         };
     }
 }
 
-#endif // NAMED_HPP_INCLUDED
+#endif // NAME_HPP
--- src/include/object.hpp
+++ src/include/object.hpp
@@ -1,63 +1,64 @@
 #ifndef OBJECT_HPP
 #define OBJECT_HPP
 
-#include <iro_object.hpp>
+#include <ro_object.hpp>
+#include <iostream>
 
-
-class Object : public IROObject
+namespace pensar_digital
 {
-    public:
-        using VersionType                   = int16_t;
-        const VersionType VERSION           = 1;
-        const VersionType INTERFACE_VERSION = 1;
-
-        /** Default constructor */
-        Object(int64_t aid = NULL_ID) : IROObject<T>(aid){};
-
-        /** Default destructor */
-        virtual ~Object() {}
-
-        /** Copy constructor
-         *  \param other Object to copy from
-         */
-        Object(const Object& other) {id = other.id}
-
-        /** Assignment operator
-         *  \param other Object to assign from
-         *  \return A reference to this
-         */
-        Object& operator=(const Object& other) { return *this; }
-
-        /** Set id
-         * \param val New value to set
-         */
-        void set_id (int64_t value) { id = value; }
-
-        std::ostream& operator << (std::ostream& os) const { return WriteToStream  (os);};
-
-    protected:
-        virtual std::istream& ReadFromStream (std::istream& is, const VersionType v)
+    namespace cpplib
+    {
+        class Object : public virtual RO_Object
         {
-            switch (v)
-            {
-                case 1:
-                    return is >> id;
-            };
-            return is;
-        };
+            public:
+                /** Default constructor */
+                Object(IdType aid = NULL_ID) : RO_Object() {};
 
-        virtual std::ostream& WriteToStream (std::ostream& os, const VersionType v) const
-        {
-            switch (v)
-            {
-                case 1:
-                    return os << id;
-            };
-            return os;
-        };
+                /** Default destructor */
+                virtual ~Object() {}
 
-    private:
-        int64_t id; //!< Member variable "id"
-};
+                /** Copy constructor
+                 *  \param other Object to copy from
+                 */
+                Object(const Object& o) {assign (o);}
 
+                virtual Object& assign (const Object& o) {id = o.get_id (); return *this;}
+
+                /** Assignment operator
+                 *  \param o Object to assign from
+                 *  \return A reference to this
+                 */
+                Object& operator=(const Object& o) { return assign (o); }
+
+                /** Set id
+                 * \param val New value to set
+                 */
+                void set_id (IdType value) { id = value; }
+
+           protected:
+                virtual std::istream& ReadFromStream (std::istream& is, const VersionType v)
+                {
+                    switch (v)
+                    {
+                        case 1:
+                            return is >> id;
+                    };
+                    return is;
+                };
+
+                virtual std::ostream& WriteToStream (std::ostream& os, const VersionType v) const
+                {
+                    switch (v)
+                    {
+                        case 1:
+                            return os << id;
+                    };
+                    return os;
+                };
+            public:
+                std::istream& operator >> (std::istream& is)       { return ReadFromStream (is, get_version ());};
+                std::ostream& operator << (std::ostream& os) const { return WriteToStream  (os, get_version ());};
+         };
+    }
+}
 #endif // OBJECT_HPP
