d7c58e129a6acb81ec6f83c8c75fc9501fd34d15
Mauricio Gomes <mgpensar@gmail.com>
2023-12-23 23:06:36 -0300

    .

---
+++ system.hpp
@@ -0,0 +1,13 @@
+// author : Mauricio Gomes
+// license: MIT (https://opensource.org/licenses/MIT)
+
+#ifdef  SYSTEM_HPP
+#define SYSTEM_HPP
+
+namespace pensar_digital
+{
+	namespace cpplib
+	{
+	}
+}
+#endif	// SYSTEM_HPP
